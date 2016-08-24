3309d921fa8331315641602fb25b2c2f378e20bf
mutilx9 <mutilx9@gmail.com>
2016-08-24 16:55:12 +0000

    mpv: added truthy check because duration could be nil

--- mpv/.config/mpv/scripts/screenshots.lua
+++ mpv/.config/mpv/scripts/screenshots.lua
@@ -1,5 +1,6 @@
 function change_screenshot_template(event)
-	if tonumber(mp.get_property('duration')) > 3600 then
+	local duration = mp.get_property('duration')
+	if duration and tonumber(duration) > 3600 then
 		mp.set_property('options/screenshot-template', '%f_snapshot_%wH.%wM.%wS_[%tY.%tm.%td_%tH.%tM.%tS]')
 	end
 end
